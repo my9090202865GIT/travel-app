0b80d9a88055246b4e9cba5f7d832fd2274560a5
my9090202865GIT <105882880+my9090202865GIT@users.noreply.github.com>
2025-03-12 11:55:53 +0530

    Amiya dev (#1)
    * background color applied
    
    * css gradiant change

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import { json_type } from '../models/json_type'
-
+import "./productCard.css"
 interface propType {
     locations?: string[],
     images: json_type[]
@@ -16,7 +16,7 @@ const ProductCard: FC<propType> = ({ locations, images }) => {
                     <span>{item.loc}</span>
                     {
                         Object.values(item.pic).map((pic) => {
-                            return < div className=" row-span-3 w-full max-w-[26rem] rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg" >
+                            return < div className="card row-span-3 w-full max-w-[26rem] rounded-xl bg-clip-border text-gray-700 shadow-lg " >
                                 <div className="relative shrink-0 mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
                                     <img
                                         src={pic}
